e0fdf51df886ecc5b25b7ee5c97f8bd14dcd6d28
C.parker <121676647+Osiego@users.noreply.github.com>
2025-02-16 03:52:42 -0800

    YES

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Page from './Home/index.jsx'; //
+import AboutUs from './about-us/index.jsx';
+import { Navbar1 } from './Home/components/Navbar1';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        
+        <Routes>
+          <Route path="/" element={<Page />} />
+          <Route path="/About-Us" element={<AboutUs />} />  {/* Define /about route */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
